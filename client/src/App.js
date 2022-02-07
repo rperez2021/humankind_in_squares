3e627585014ca7de964c05be9abf86021c02c4ee
Roberto Perez <rob.perez.flores@gmail.com>
2022-02-06 22:17:31 -0800

    Styled home page

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" height="100px" width="100px" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Humankind In Squares
+          <br></br>
+          Feel free to Add!
         </p>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+          
+        
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      
       </header>
       <Grid></Grid>
     </div>
--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -2,48 +2,47 @@ import './Grid.css';
 
 function Grid() {
     return (
-        <div className="Grid">
-            <div className=' grid grid-cols-12 gap-4'>
-                <div className='rounded'>01</div>
-                <div className='rounded'>02</div>
-                <div className='rounded'>03</div>
-                <div className='rounded'>04</div>
-                <div className='rounded'>05</div>
-                <div className='rounded'>06</div>
-                <div className='rounded'>07</div>
-                <div className='rounded'>08</div>
-                <div className='rounded'>09</div>
-                <div className='rounded'>10</div>
-                <div className='rounded'>11</div>
-                <div className='rounded'>12</div>
-            </div>
-            <div className=' grid grid-cols-12 gap-4'>
-                <div className='rounded'>01</div>
-                <div className='rounded'>02</div>
-                <div className='rounded'>03</div>
-                <div className='rounded'>04</div>
-                <div className='rounded'>05</div>
-                <div className='rounded'>06</div>
-                <div className='rounded'>07</div>
-                <div className='rounded'>08</div>
-                <div className='rounded'>09</div>
-                <div className='rounded'>10</div>
-                <div className='rounded'>11</div>
-                <div className='rounded'>12</div>
-            </div>
-            <div className=' grid grid-cols-12 gap-4'>
-                <div className='rounded'>01</div>
-                <div className='rounded'>02</div>
-                <div className='rounded'>03</div>
-                <div className='rounded'>04</div>
-                <div className='rounded'>05</div>
-                <div className='rounded'>06</div>
-                <div className='rounded'>07</div>
-                <div className='rounded'>08</div>
-                <div className='rounded'>09</div>
-                <div className='rounded'>10</div>
-                <div className='rounded'>11</div>
-                <div className='rounded'>12</div>
+        <div className='container mx-auto px-4 my-20'>
+            <div className="Grid">
+                <div className=' grid grid-cols-2 md:grid-cols-5 lg:grid-cols-10 gap-4 my-5'>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>01</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>02</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>03</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>04</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>05</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>06</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>07</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>08</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>09</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>10</div>
+
+                </div>
+                <div className=' grid grid-cols-2 md:grid-cols-5 lg:grid-cols-10 gap-4 my-5'>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>11</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>12</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>13</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>14</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>15</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>16</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>17</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>18</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>19</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>20</div>
+
+                </div>
+                <div className=' grid grid-cols-2 md:grid-cols-5 lg:grid-cols-10 gap-4 my-5'>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>21</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>22</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>23</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>24</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>25</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>26</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>27</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>28</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>29</div>
+                    <div className='border rounded border-indigo-600 p-5 shadow shadow-current hover:shadow-amber-500'>30</div>
+
+                </div>
             </div>
         </div>
     );
