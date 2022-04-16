b9045d01aedcd033a8805bbbeb00c932111676a5
Roberto Perez <rob.perez.flores@gmail.com>
2022-04-15 19:41:24 -0700

    Added ESLint to Project Fixed Linting Issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Grid from './components/Grid';
 
+/**
+ * Creates the grid element.
+ * @return {function} Returns the App Parent Element
+ */
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" height="100px" width="100px" />
+        <img src={logo} className="App-logo"
+          alt="logo" height="100px" width="100px" />
         <p>
           Welcome to Humankind In Squares
           <br></br>
           Feel free to Add!
         </p>
-          
-        
-
-        
-      
       </header>
       <Grid></Grid>
     </div>
